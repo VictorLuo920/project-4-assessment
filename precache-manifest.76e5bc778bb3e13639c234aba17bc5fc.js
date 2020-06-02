0255c7c01e6725bd35272a6ec37969346b2b1484
Victor Luo <victorluo920@gmail.com>
2020-06-02 14:15:55 -0700

    Updates

---
+++ precache-manifest.76e5bc778bb3e13639c234aba17bc5fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557b02efbedb85f88dd2f3c9a5a487c1",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "8881250d61ce13c278ba",
+    "url": "/project-4-assessment/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8881250d61ce13c278ba",
+    "url": "/project-4-assessment/static/js/main.a812329f.chunk.js"
+  },
+  {
+    "revision": "11176ca57580c991af7e",
+    "url": "/project-4-assessment/static/js/runtime-main.548f4f76.js"
+  }
+]);
